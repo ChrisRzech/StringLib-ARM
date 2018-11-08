31f7a5269b1aee8d5179175f936f157e6c140ea6
PullusFurtivus <lucasd1099@gmail.com>
2018-11-08 13:41:23 -0800

    Finished string_replace

--- start.s
+++ start.s
@@ -1,18 +1,17 @@
 .data
-string1:	.asciz	"0123456789\n"
+string1:	.asciz	"0123123123\n"
 error:		.asciz	"Error\n"
 
 .text
 .global	_start
 _start:
-	@ldr	R1,=string1
-	@bl	string_print
-	
-	mov	R3,#'6'
-	mov	R2,#3
 	ldr	R1,=string1
-	bl	string_indexOf_2
-	blvc	v_dec
+	bl	string_print
+	mov	R2,#'A'
+	mov	R3,#'0'
+	bl	string_replace
+	bl	string_print
+	
 
 	mov	R7,#1
 	svc	0
