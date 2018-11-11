1b0b85085d1c0423864398786e69999dff6aece1
Christopher Rzechula <christopherrzechula@gmail.com>
2018-11-10 20:23:51 -0800

    Added function zero_regs

---
+++ zero_regs.s
@@ -0,0 +1,28 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Loads all registers with zeros @
+@================@@@@@@@@@@@@@@@@@
+@ Pre-condition  @
+@ All: --        @
+@================@
+@ Post-condition @
+@ All: 0         @
+@@@@@@@@@@@@@@@@@@
+.text
+.global zero_regs
+zero_regs:
+	mov	R0,#0
+	mov	R1,#0
+	mov	R2,#0
+	mov	R3,#0
+	mov	R4,#0
+	mov	R5,#0
+	mov	R6,#0
+	mov	R7,#0
+	mov	R8,#0
+	mov	R9,#0
+	mov	R10,#0
+	mov	R11,#0
+	mov	R12,#0
+	bx	LR
+.end
+
