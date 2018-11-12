f01242670c7809f383c7cb99e3e77f45c77dfc58
Christopher Rzechula <christopherrzechula@gmail.com>
2018-11-11 19:13:32 -0800

    Added a function that outputs a \n

---
+++ v_endl.s
@@ -0,0 +1,24 @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Outputs a newline character @
+@================@@@@@@@@@@@@@@
+@ Pre-condition  @
+@ R1: --         @
+@================@
+@ Post-condition @
+@ R1: --         @
+@@@@@@@@@@@@@@@@@@
+.data
+endl:	.asciz	"\n"
+
+.text
+.global v_endl
+v_endl:
+	push	{R1,LR}
+
+	ldr	R1,=endl
+	bl	string_print
+
+	pop	{R1,LR}
+	bx	LR
+.end
+
